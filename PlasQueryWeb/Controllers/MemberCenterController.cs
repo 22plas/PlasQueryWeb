6bd3b5e2007b5ab6138c284aa24ec8db6d80a2cb
Administrator <Administrator@DKS-20161101KYH>
2019-04-20 17:25:04 +0800

    addmembercenters

--- PlasQueryWeb/Controllers/MemberCenterController.cs
+++ PlasQueryWeb/Controllers/MemberCenterController.cs
@@ -14,23 +14,89 @@ namespace PlasQueryWeb.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         //
-        // GET: /会员中心/
+        // GET: /会员中心首页/
         public ActionResult Index()
         {
             return View();
         }
-
+        //登录
         public ActionResult Login()
         {
             return View();
         }
-
+        //注册
         public ActionResult Register()
         {
             return View();
         }
+        //用户信息
+        public ActionResult UserInfo() {
+            return View();
+        }
+        //公司资料
+        public ActionResult CompanyInfo()
+        {
+            return View();
+        }
+        //新增公司资料
+        public ActionResult CompanyInfoCreate()
+        {
+            return View();
+        }
+        //收货地址
+        public ActionResult DeliveryAddress() {
+            return View();
+        }
+        //收货地址
+        public ActionResult DeliveryAddressCreate()
+        {
+            return View();
+        }
+        //物料收藏
+        public ActionResult MaterialCollection() {
+            return View();
+        }
+        //物料收藏
+        public ActionResult MaterialLook()
+        {
+            return View();
+        }
+        //我要报价
+        public ActionResult SellerOffer() {
+            return View();
+        }
+        //新增卖家报价
+        public ActionResult SellerOfferCreate()
+        {
+            return View();
+        }
 
+        //买家询价
+        public ActionResult BuyerOffer()
+        {
+            return View();
+        }
+        //新增买家询价
+        public ActionResult BuyerOfferCreate()
+        {
+            return View();
+        }
 
+        //收到的报价
+        public ActionResult GetSellerOffer()
+        {
+            return View();
+        }
+        //收到的询价
+        public ActionResult GetBuyerOffer()
+        {
+            return View();
+        }
+
+        //行情浏览记录
+        public ActionResult QuotationRecord() {
+            return View();
+        }
         /// <summary>
         /// 发送验证码
         /// </summary>
